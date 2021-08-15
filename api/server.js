5b0f2fdee83cdfd34d7910f633a9181fcd185e2e
thecuriousones <tamaraleonard46@gmail.com>
2021-08-15 11:17:51 -0400

    project endpoints comopleted

--- api/server.js
+++ api/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const server = express();
+const projectRouter = require('./projects/projects-router.js')
+const projectMiddleware = require('./projects/projects-middleware.js')
+const actionRouter = require('./actions/actions-router.js')
+const actionMiddleware = require('./actions/actions-middlware.js')
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+server.use(express.json());
+server.use('/api/projects', projectRouter)
+// server.use('/api/actions', actionMiddleware, actionRouter)
+
 
 module.exports = server;
