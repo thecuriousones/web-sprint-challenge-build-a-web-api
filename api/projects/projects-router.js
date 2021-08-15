854fd173b7ca742030866b9f479b926a5ad00e53
thecuriousones <tamaraleonard46@gmail.com>
2021-08-15 13:05:23 -0400

    more

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -38,8 +38,8 @@ router.get('/:id', validateProjectId, (req,res)=>{
 
 router.post('/', validateProject, (req,res)=>{
     const newProject = req.body
-    if(!newProject.name || !newProject.description || !newProject.completed ){
-        res.status(400).json({message: "Please provide a name, description, and complete status for new project"})
+    if(!newProject.name || !newProject.description ){
+        res.status(400).json({message: "Please provide a name and description for new project"})
     }else{
         Project.insert(newProject)
             .then(project =>{
